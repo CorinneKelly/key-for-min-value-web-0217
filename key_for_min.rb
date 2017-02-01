522cafc93f9dff9f539a54ab5922340b16270787
Corinne Kelly <corinnemariekelly@gmail.com>
2017-02-01 19:08:42 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+  min_value = 10000
+  #set default super high so all values would be less
+  # would be better to set default to first value but could not find a way to do this without using the .values method
+  name_hash.each do |key, value|
+    if value < min_value
+      min_value = value
+    end
+  end
+  name_hash.index(min_value)
 end
